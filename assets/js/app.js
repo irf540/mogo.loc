e4916e4bd0df50101295934a2b18d8a38ea862c9
Dmitriy <d.podluzhnyi@gmail.com>
2020-10-20 20:59:24 +0300

    finish

--- assets/js/app.js
+++ assets/js/app.js
@@ -64,9 +64,21 @@ $("[data-collapse]").on("click", function(event){
     var $this = $(this),
     blockId = $this.data('collapse');   
 
-    $(blockId).slideToggle();
+
+    $this.toggleClass("active");
 
 });
+
+/* Slider */
+
+$("[data-slider]").slick({
+    infinite: true,
+    fade: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+
+
 
 
 
