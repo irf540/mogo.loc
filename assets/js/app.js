23253a900870b47ec4a1130459bf77f1e7f668d7
Dmitriy <d.podluzhnyi@gmail.com>
2020-10-20 17:01:44 +0300

    burger)menu_jq

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ $(function () {
 
 
 
-    /* Header fixed */
+    /* Fixed header  */
     checkScroll(scrollOffset)
 
     $(window).on("scroll", function () {
@@ -24,5 +24,44 @@ $(function () {
             header.removeClass("fixed");
         }
     }
+
+
+/*  Smooth scroll */
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("nav a").removeClass("active");
+        $this.addClass("active");    
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    }); 
+
+
+/*  Menu nav toggle */
+
+$("#nav_toggle").on("click", function(event) {
+    event.preventDefault();
+
+    $(this).toggleClass("active");
+    $("#nav").toggleClass("active");
+
+
+});
+
+
+
+
+
+
+
+
+
 
 });
